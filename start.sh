5ade24355755ee89c9e484a1c98cbc6a39feb71f
Ali-Ghasemi-Tech <thelastblade30@gmail.com>
2025-05-19 20:25:05 +0330

    change running port

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ echo "Importing hospitals data..."
 python API/import_hospitals.py
 
 echo "Starting FastAPI app..."
-uvicorn API.main:app --host 0.0.0.0 --port 8000
+uvicorn API.main:app --host 0.0.0.0 
