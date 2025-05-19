0c9329690e5e2d2b2f08f629066f6c5d15a5134a
Ali-Ghasemi-Tech <thelastblade30@gmail.com>
2025-05-19 22:28:39 +0330

    add cors for expo app

--- API/main.py
+++ API/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from API.views.hospitals import router as hospital_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Hospital Location API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register the router
 app.include_router(hospital_router)
