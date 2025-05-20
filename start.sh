53415f3db67090a0495121d412b1763fa0c60bf0
Ali-Ghasemi-Tech <thelastblade30@gmail.com>
2025-05-20 21:38:33 +0330

    remove the alembic command from docker cmd

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e  # Stop on error
 
-echo "Running Alembic migrations..."
-alembic upgrade head
+# echo "Running Alembic migrations..."
+# alembic upgrade head
 
 echo "Starting FastAPI app..."
 uvicorn API.main:app --host 0.0.0.0 --port 3000
